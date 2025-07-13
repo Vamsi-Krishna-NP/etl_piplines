92549d43c099010cfdf2e77dc1451ff44bd7b5c5
Vamsi-Krishna-NP <mr.vkjilla2024@gmail.com>
2025-07-13 18:51:34 +0530

    ETL Process is completed

---
+++ push_data.py
@@ -0,0 +1,71 @@
+import os
+import sys
+import json
+import certifi
+import pymongo
+import pandas as pd
+import numpy as np
+from dotenv import load_dotenv
+from networksecurity.logging import logger
+from networksecurity.exception import NetworkSecurityException
+
+load_dotenv()
+
+uri = os.getenv("MONGO_DB_URI")
+ca = certifi.where()
+
+class NetworkDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    def cv_to_json_convertor(self, file):
+        try:
+            data = pd.read_csv(file)
+            data.reset_index(drop=True, inplace=True)
+            records = list(json.loads(data.T.to_json()).values())
+            logger.info(f"Data extracted from {file} and converted to JSON format")
+            print(f"Data extracted from {file} and converted to JSON format")
+            
+            return records
+            
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    def push_data_to_mongo(self,records,database,collection):
+        try:
+            self.records = records
+            self.database = database
+            self.collection = collection
+            
+            self.client = pymongo.MongoClient(uri, tlsCAFile=ca)
+            self.database = self.client[self.database]
+            self.collection = self.database[self.collection]
+            self.collection.insert_many(self.records)
+            
+            logger.info(f"Data pushed to MongoDB collection: {self.collection.name} in database: {self.database.name}")
+            print(f"Data pushed to MongoDB collection: {self.collection.name} in database: {self.database.name}")
+            
+            return len(self.records)
+            
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+if __name__ == "__main__":
+    try:
+        file = "Network_data\phisingData.csv"  # Example file path
+        database = "network_security"
+        collection = "network_data"
+        
+        extractor = NetworkDataExtract()
+        records = extractor.cv_to_json_convertor(file)
+        count = extractor.push_data_to_mongo(records, database, collection)
+        
+        print(f"Total records pushed: {count}")
+        
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+    
+    
